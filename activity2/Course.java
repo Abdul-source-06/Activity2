62fe3c3e668c320fb3eb5cd33780266bcb45584c
Abdul-source-06 <149016670+Abdul-source-06@users.noreply.github.com>
2024-04-03 17:42:02 +0200

    Add files via upload

---
+++ activity2/Course.java
@@ -0,0 +1,26 @@
+package activity2;
+
+
+public class Course {
+    private Student[] students;
+
+    public int countStudents() {
+        if (students != null) {
+            return students.length;
+        } else {
+            return 0;
+        }
+    }
+
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
